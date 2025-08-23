0296d6f34de26076ea57fab9a606ced51d5df991
pelzmagic <akinloyepelz@gmail.com>
2025-08-23 15:36:22 +0100

    add page

--- src/pages/notes.tsx
+++ src/pages/notes.tsx
@@ -6,13 +6,26 @@ export default function Notes() {
         <p className="font-pacifico text-black text-[23px] tracking-[-2%] font-normal">notes</p>
       </div>
       <div className="py-3 px-4 border-t border-neutral-200 absolute bottom-0 w-full">
-        <div className="flex items-center justify-between">
-          <img src="/Home.png" alt="home icon" className="w-6 h-6" />
-          <img src="/Search.png" alt="search icon" className="w-6 h-6" />
-          <img src="/Archive.png" alt="archive icon" className="w-6 h-6" />
-          <img src="/Tag.png" alt="tag icon" className="w-6 h-6" />
-          <img src="/Setting.png" alt="setting icon" className="w-6 h-6" />
+        <div className="flex items-center">
+          <div className="py-1 w-[68.6px] hover:bg-blue-50 rounded-sm">
+            <img src="/Home.png" alt="home icon" className="w-6 h-6 mx-auto" />
+          </div>
+          <div className="py-1 w-[68.6px] hover:bg-blue-50 rounded-sm">
+            <img src="/Search.png" alt="search icon" className="w-6 h-6 mx-auto" />
+          </div>
+          <div className="py-1 w-[68.6px] hover:bg-blue-50 rounded-sm">
+            <img src="/Archive.png" alt="archive icon" className="w-6 h-6 mx-auto" />
+          </div>
+          <div className="py-1 w-[68.6px] hover:bg-blue-50 rounded-sm">
+            <img src="/Tag.png" alt="tag icon" className="w-6 h-6 mx-auto" />
+          </div>
+          <div className="py-1 w-[68.6px] hover:bg-blue-50 rounded-sm">
+            <img src="/Setting.png" alt="setting icon" className="w-6 h-6 mx-auto" />
+          </div>
         </div>
+      </div>
+      <div className="w-12 h-12 bg-blue-500 rounded-full flex items-center justify-center">
+        <img src="/Plus.png" alt="plus icon" className="w-8 h-8" />
       </div>
     </div>
   );
