3a8618a95f7369fa257e620b50dd576688904184
pelzmagic <akinloyepelz@gmail.com>
2025-08-23 15:06:05 +0100

    add mobile version

--- src/pages/notes.tsx
+++ src/pages/notes.tsx
@@ -5,8 +5,14 @@ export default function Notes() {
         <img src="/Feather.png" alt="logo icon" className="w-7 h-7" />
         <p className="font-pacifico text-black text-[23px] tracking-[-2%] font-normal">notes</p>
       </div>
-      <div>
-        <img src="/"
+      <div className="py-3 px-4 border-t border-neutral-200 absolute bottom-0 w-full">
+        <div className="flex items-center justify-between">
+          <img src="/Home.png" alt="home icon" className="w-6 h-6" />
+          <img src="/Search.png" alt="search icon" className="w-6 h-6" />
+          <img src="/Archive.png" alt="archive icon" className="w-6 h-6" />
+          <img src="/Tag.png" alt="tag icon" className="w-6 h-6" />
+          <img src="/Setting.png" alt="setting icon" className="w-6 h-6" />
+        </div>
       </div>
     </div>
   );
