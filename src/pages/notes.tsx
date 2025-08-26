acd20db632c95707fd26b07fc74d101444c89f21
pelzmagic <akinloyepelz@gmail.com>
2025-08-27 00:11:26 +0100

    add page

--- src/pages/notes.tsx
+++ src/pages/notes.tsx
@@ -1,6 +1,27 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { supabase } from "@/supabase-client";
+import { useEffect } from "react";
+
+type Note = {
+  title: "string";
+  created_at: "string";
+  id: "string";
+  content: "string";
+};
 
 export default function Notes() {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const { data, error } = await supabase.from("user-notes").select("*").order("created_at", { ascending: false });
+    if (error) console.error(error);
+    else setNotes(data);
+  }
   return (
     <div className="flex flex-col h-screen overflow-x-hidden relative">
       <div className="py-3 px-4 bg-neutral-100 flex items-center gap-3">
@@ -33,6 +54,19 @@ export default function Notes() {
       </Link>
       <div className="py-5 px-4 flex-1 overflow-y-auto scrollbar-hidden flex flex-col gap-4 rounded-t-md">
         <h1 className="font-inter font-2xl font-bold leading-[120%] tracking-[-0.5px] text-neutral-950">All Notes</h1>
+        <div className="flex flex-col gap-[9px]">
+          {notes.map((note: Note) => (
+            <div className="p-2 flex flex-col gap-3 border-b border-neutral-200" key={note.id}>
+              <h1 className="text-neutral-950 text-base leading-[120%] tracking-[-0.3px] font-inter font-bold">{note.title}</h1>
+              <div>
+                <div className="px-[6px] py-[2px] bg-neutral-200 rounded-sm">
+                  <p className="text-neutral-950 text-xs leading-[120%] tracking-[-0.2px] font-inter font-normal">Dev</p>
+                </div>
+              </div>
+              <p>{note.created_at}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
