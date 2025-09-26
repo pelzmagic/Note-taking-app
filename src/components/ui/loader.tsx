f5c9e27a0be0d62426dcde8befa2130779000522
pelzmagic <akinloyepelz@gmail.com>
2025-09-26 12:17:06 +0100

    add loader

---
+++ src/components/ui/loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="absolute bg-slate-200/50 backdrop-blur-sm flex items-center justify-center inset-0">
+      <div className="loader"></div>
+    </div>
+  );
+}
