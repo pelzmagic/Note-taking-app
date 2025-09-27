fcd7396d02005ce616364ebd58f0f7c8718b3406
pelzmagic <akinloyepelz@gmail.com>
2025-09-27 05:21:48 +0100

    persist session

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -3,4 +3,9 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://axnpxjjaxpnfrpbcawcu.supabase.co";
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY as string;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+  },
+});
